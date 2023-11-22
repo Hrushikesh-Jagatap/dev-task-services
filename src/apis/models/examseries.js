d3d03ee83a9c1a8e19549c6fce485e85b3850e3d
amit00986 <aryan.amit0824@gmail.com>
2023-11-22 14:47:29 +0530

    chnages

--- src/apis/models/examseries.js
+++ src/apis/models/examseries.js
@@ -1,17 +1,38 @@
 const mongoose = require('mongoose');
 
 const examseries = new mongoose.Schema({
-  admin_id:String,
-  price:String,
-  questionType:String,
-  question:String,
-  level:String,
-  options:[String],
-  correctAnswer:String,
-  solution:String,
+  admin_id: {
+    type: String
+  },
+
+  price: {
+    type: String
+  },
+
+  questionType: {
+    type: String
+  },
+
+  question: {
+    type: String
+  },
+
+  level: {
+    type: String
+  },
+
+  options: [String],
+
+  correctAnswer: {
+    type: String
+  },
+
+  solution: {
+    type: String
+  },
 
 },
-{ timestamps: true }); 
+  { timestamps: true });
 
 const Exam_series = mongoose.model('Exam_series', examseries);
 
