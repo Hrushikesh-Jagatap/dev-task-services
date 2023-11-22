366d074eac712023c71cdcb7103b04ab70f0ed1a
amit00986 <aryan.amit0824@gmail.com>
2023-11-22 14:42:53 +0530

    changes

--- src/apis/models/index.js
+++ src/apis/models/index.js
@@ -2,10 +2,12 @@ const Assignment = require('./Assignment');
 const Exam = require('./Exam');
 const Resources = require('./Resources');
 const que = require('./que');
+const ExamSeries = require('./examseries');
 
 module.exports = {
     Assignment,
     Resources,
     Exam,
     que,
+    ExamSeries
 }
