1a178ff8c16deb20715637b6aa8b0ebd9bb5f803
amit00986 <aryan.amit0824@gmail.com>
2023-11-22 14:50:44 +0530

    chnages

--- src/apis/models/que.js
+++ src/apis/models/que.js
@@ -1,29 +1,62 @@
 const mongoose = require('mongoose');
 
 const queschema = new mongoose.Schema({
-  class:String,
-  sub:String,
-  lang:String,
-  actual_ques:String,
-  status:String,
-  duration:String,
-  studnetid:String,
-  name:{type:String},
-    profileimage:{type:String}, 
-  req_teacherid:[{
-    teacherid:String,
-    meetingurl:{type:String,default:null},
-    status:{
-     type:String,
-    default:"requested"
+
+  class: {
+    type: String
+  },
+
+  sub: {
+    type: String
+  },
+
+  lang: {
+    type: String
+  },
+
+  actual_ques: {
+    type: String,
+  },
+
+  status: {
+    type: String
+  },
+
+  duration: {
+    type: String
+  },
+
+  studnetid: {
+    type: String,
+  },
+
+  name: {
+    type: String
+  },
+
+  profileimage: {
+    type: String
+  },
+
+  req_teacherid: [{
+    teacherid: String,
+    meetingurl: { type: String, default: null },
+    status: {
+      type: String,
+      default: "requested"
     },
-    
-    flag:{type:Boolean,default:true},
-     
+
+    flag: {
+      type: Boolean,
+      default: true
+    },
+
   }],
-  review:String,
+  review: {
+    type: String
+  },
 },
-{ timestamps: true }); 
+  { timestamps: true });
 
 const que = mongoose.model('que', queschema);
 
