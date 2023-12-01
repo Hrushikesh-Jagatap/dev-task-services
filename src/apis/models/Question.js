a2ab93d3487a7db5602a4b45aee4ff2f44fa2000
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-01 17:09:58 +0530

    Create Question.js

---
+++ src/apis/models/Question.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+// Define a schema for the Question model (used within Exam)
+const questionSchema = new mongoose.Schema({
+    examId: {
+        type: String
+    },
+    
+    questionType: {
+        type: String,
+        // required: true
+    },
+    question: String,
+    level: String,
+    options: [String],
+    correctAnswer: Number, // Store the index of the correct option
+    solution: String,
+    markPerQues: {
+        type: Number
+    },
+});
+const Question = mongoose.model('Question', questionSchema);
+module.exports = Question;
