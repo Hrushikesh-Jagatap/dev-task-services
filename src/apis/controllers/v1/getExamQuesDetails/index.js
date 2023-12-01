3b426eb1c45f45ccf896a2477b41d38583290b2e
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-01 17:15:20 +0530

    Update index.js

--- src/apis/controllers/v1/getExamQuesDetails/index.js
+++ src/apis/controllers/v1/getExamQuesDetails/index.js
@@ -3,7 +3,6 @@ const QuesService = require('@root/src/apis/services/v1/getExamQuesDetails');
 const { HttpResponseHandler } = require('intelli-utility');
 // Controller function to get a single batch by  teacherid
 
-
 const getExamQuesDetails = async (req, res, next) => {
     try {
         const examId = req.query.examId
