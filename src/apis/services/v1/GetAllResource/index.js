07b8d037d2d6cfb7f6ac0dec93910df4e34e2f60
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-01 22:57:03 +0530

    Update index.js

--- src/apis/services/v1/GetAllResource/index.js
+++ src/apis/services/v1/GetAllResource/index.js
@@ -2,10 +2,11 @@ const Resource = require('@models/Resources');
 
 const getAllResources = async () => {
   try {
-    const resources = await Resource.find({  });
+   const resources = await Resource.find({ isdelete: false });
     return resources;
   } catch (error) {
-    throw error;
+console.error('Failed to fetch resources:', error);
+    throw new Error('Failed to fetch resources');
   }
 };
 
