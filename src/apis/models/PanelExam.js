5a71d03d4d42b71a0871737ce57c6c0580dcbf4b
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-01 14:57:08 +0530

    Update PanelExam.js

--- src/apis/models/PanelExam.js
+++ src/apis/models/PanelExam.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PanelExamSchema = new Schema({
+    examName: {
+        type:String
+    },
     userId: {
         type: String
     },
