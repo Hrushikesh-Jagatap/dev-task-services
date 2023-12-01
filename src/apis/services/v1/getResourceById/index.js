267ae2d08c386c83e4756d0b818b22c9ec58a0a1
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-01 17:21:20 +0530

    Update index.js

--- src/apis/services/v1/getResourceById/index.js
+++ src/apis/services/v1/getResourceById/index.js
@@ -1,9 +1,11 @@
 const ResourceData = require('@models/Resources');
+const mongoose = require('mongoose');
 
 // Service function to get a single resource by Id
 const getResourceDetailsById = async (resourceId) => {
   try {
-    const resource = await ResourceData.findOne({ _id:  resourceId});
+     const objectId = mongoose.Types.ObjectId(resourceId);
+    const resource = await ResourceData.findOne({ _id:  objectId});
     return resource;
   } catch (error) {
     throw new Error('Failed to get resource Details');
