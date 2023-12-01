3a3c2a6d13f3919456c39ac8795295d46a8d7129
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-01 16:04:47 +0530

    Create index.js

---
+++ src/apis/controllers/v1/RatingforResource/index.js
@@ -0,0 +1,19 @@
+const ResourceService = require('@root/src/apis/services/v1/RatingforResource');
+const { HttpResponseHandler } = require('intelli-utility');
+// update /exam
+const updateResourceById = async (req, res, next) => {
+//   const { userId } = req.body;
+  
+  try {
+    const updateResource = await ResourceService.updateResourceById(req.params.id, req.body);
+    if (!updateResource) {
+      return HttpResponseHandler.success(req, res, updateResource);
+    }
+    return HttpResponseHandler.success(req, res, updateResource);
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = {
+    updateResourceById,
+};
