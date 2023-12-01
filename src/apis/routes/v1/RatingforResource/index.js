02e1d97d2cc345c2925ed27969b2317da0c43f27
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-01 16:06:11 +0530

    Update index.js

--- src/apis/routes/v1/RatingforResource/index.js
+++ src/apis/routes/v1/RatingforResource/index.js
@@ -1,1 +1,12 @@
 
+const express = require('express');
+const router = express.Router();
+const updateResourceforratingController = require('@controllers/v1/RatingforResource')
+router.put('/resource/rating/:id', async(req, res, next) => {
+    try {
+        const result = await updateResourceforratingController.updateResourceById(req, res, next);
+    } catch (error) {
+        next(error);
+    } 
+});
+module.exports = router;
