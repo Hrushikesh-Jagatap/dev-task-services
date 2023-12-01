d57bc4b6f3fd6a95eff48f6ade52c7266572708c
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-01 13:49:01 +0530

    Update index.js

--- src/apis/services/v1/publishedResourceById/index.js
+++ src/apis/services/v1/publishedResourceById/index.js
@@ -9,38 +9,38 @@ const publishResourceById = async (resourceId, publishStatus) => {
 
     let resource = await ResourceData.findById(mongoose.Types.ObjectId(resourceId));
 
-    if (publishStatus == 'true') {
-      //send notification to teacher_> userId
-      const NotificationData = {
-        userId: resource.userId,
-        appName: 'teacherApp',
-        data: {
-          message: 'Your Resource has been Added',
-        },
-        body: 'Your Resource has been Added',
-        title: 'Your Resource has been Added'
-      };
+    // if (publishStatus == 'true') {
+    //   //send notification to teacher_> userId
+    //   const NotificationData = {
+    //     userId: resource.userId,
+    //     appName: 'teacherApp',
+    //     data: {
+    //       message: 'Your Resource has been Added',
+    //     },
+    //     body: 'Your Resource has been Added',
+    //     title: 'Your Resource has been Added'
+    //   };
 
-      const teacher = await pushNotification(NotificationData) // send  Notification to teacher 
+    //   const teacher = await pushNotification(NotificationData) // send  Notification to teacher 
 
-      // // class and subject // need all student Ids to send Notification
-      const students = await getStudentsBySubjectAndClass(resource.class, resource.subject);
-      // // send notification to all student
-      notifyStudentsForResourceUpdation(students);
-    }
-    else {
+    //   // // class and subject // need all student Ids to send Notification
+    //   const students = await getStudentsBySubjectAndClass(resource.class, resource.subject);
+    //   // // send notification to all student
+    //   notifyStudentsForResourceUpdation(students);
+    // }
+    // else {
 
-      const NotificationData = {
-        userId: resource.userId,
-        appName: 'teacherApp',
-        data: {
-          message: 'Your Resource has been not Added',
-        },
-        body: 'Your Resource has been not Added',
-        title: 'Your Resource has been not Added'
-      };
+    //   const NotificationData = {
+    //     userId: resource.userId,
+    //     appName: 'teacherApp',
+    //     data: {
+    //       message: 'Your Resource has been not Added',
+    //     },
+    //     body: 'Your Resource has been not Added',
+    //     title: 'Your Resource has been not Added'
+    //   };
 
-      const teacher = await pushNotification(NotificationData) // send  Notification to teacher 
+      // const teacher = await pushNotification(NotificationData) // send  Notification to teacher 
     }
     resource.publishStatus = publishStatus;
     await resource.save();
