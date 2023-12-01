696391bbf92318db4d6273babf2c7cd4b9bcba3f
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-01 22:57:56 +0530

    Update index.js

--- src/apis/services/v1/getAllResourcesByUserId/index.js
+++ src/apis/services/v1/getAllResourcesByUserId/index.js
@@ -2,10 +2,10 @@ const Resource = require('@models/Resources');
 
 const getAllResources = async (userId) => {
   try {
-    const resources = await Resource.find({ userId });
+     const resources = await Resource.find({ userId, isdelete: false });
     return resources;
   } catch (error) {
-    throw error;
+    throw new Error('Failed to fetch resources');
   }
 };
 
