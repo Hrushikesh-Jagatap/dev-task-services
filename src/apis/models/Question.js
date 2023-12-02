c9e237696076621f5f846d5075f0dbfc3fa5c4e7
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-02 15:04:39 +0530

    Update Question.js

--- src/apis/models/Question.js
+++ src/apis/models/Question.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
+
 // Define a schema for the Question model (used within Exam)
 const questionSchema = new mongoose.Schema({
     examId: {
         type: String
     },
-    
+
     questionType: {
         type: String,
         // required: true
@@ -17,14 +18,23 @@ const questionSchema = new mongoose.Schema({
     markPerQues: {
         type: Number
     },
-     createdAt: {
+
+    createdAt: {
         type: Date,
         default: Date.now
     },
     updatedAt: {
         type: Date,
         default: Date.now
-    }
+    },
+    
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+
 });
+
 const Question = mongoose.model('Question', questionSchema);
+
 module.exports = Question;
