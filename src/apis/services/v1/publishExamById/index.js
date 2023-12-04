cc811704fdfeba893328e8341da79bed0e1086a0
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-04 14:01:50 +0530

    Update index.js

--- src/apis/services/v1/publishExamById/index.js
+++ src/apis/services/v1/publishExamById/index.js
@@ -1,10 +1,9 @@
 const ExamData = require('@models/Exam');
 
 // Service function to get a single teacher by ID
-const publishExamById = async (examId) => {
-  let publishStatus = 'true';
+const publishExamById = async (examId, publishStatus) => {
   try {
-    const exam = await ExamData.findByIdAndUpdate({ _id: examId }, { publishStatus });
+    const exam = await ExamData.findByIdAndUpdate({ _id: examId }, { publishStatus: publishStatus });
     return exam;
   } catch (error) {
     throw new Error('Failed to publish  ExamData');
