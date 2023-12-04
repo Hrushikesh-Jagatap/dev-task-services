2683ebbdaa2a6c96602236e1d531bff6dfd981d6
Hrushikesh-Jagatap <145649381+Hrushikesh-Jagatap@users.noreply.github.com>
2023-12-04 14:01:16 +0530

    Update index.js

--- src/apis/controllers/v1/publishExamById/index.js
+++ src/apis/controllers/v1/publishExamById/index.js
@@ -1,10 +1,12 @@
 const ExamService = require('@root/src/apis/services/v1/publishExamById');
 const { HttpResponseHandler } = require('intelli-utility');
 
-// Controller function to get a single teacher by userID
+// Controller function to get 
 const publishExamById = async (req, res, next) => {
     try {
-        const Exam = await ExamService.publishExamById(req.params.Id);
+        const publishStatus = req.body.publishStatus;
+
+        const Exam = await ExamService.publishExamById(req.params.Id, publishStatus);
 
         if (!Exam) {
             return HttpResponseHandler.success(req, res, Exam);
